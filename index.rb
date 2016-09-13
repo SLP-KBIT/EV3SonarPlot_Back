79774d18373c312080e32fa5981e0858eee02149
gembaf <gembaf.github@gmail.com>
2016-09-13 14:23:22 +0900

    散布図のサンプルを追加

--- index.rb
+++ index.rb
@@ -6,6 +6,40 @@ cgi = CGI.new
 puts cgi.header('charset' => 'UTF-8')
 
 puts <<EOS
-<h2>hoge</h2>
+<html>
+  <head>
+    <title>EV3SonarPlot</title>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+     <script type="text/javascript">
+       google.charts.load('current', {'packages':['corechart']});
+       google.charts.setOnLoadCallback(drawChart);
+
+       function drawChart() {
+         var data = new google.visualization.arrayToDataTable([
+           ['X', 'Y'],
+           [8, 12],
+           [4, 5],
+           [11, 14],
+           [4, 5],
+           [3, 4],
+           [7, 7]
+         ]);
+
+         var options = {
+           title: 'Sonar Plot',
+           hAxis: { minValue: -50, maxValue: 50 },
+           vAxis: { minValue: -50, maxValue: 50 },
+           legend: 'none'
+         };
+
+         var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
+         chart.draw(data, options);
+       }
+    </script>
+  </head>
+  <body>
+    <div id="chart_div" style="width: 600px; height: 600px;" />
+  </body>
+</html>
 EOS
 
